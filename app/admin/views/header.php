e6979b89fe7953e68bee04a2e18871eb822066e5
quangson23 <nguyenquangson23022004@gmail.com>
2023-11-20 21:02:37 +0700

    update

--- app/admin/views/header.php
+++ app/admin/views/header.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../admin/public/css/style.css">
+   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+
     <title>Quản trị</title>
 </head>
 
@@ -12,15 +15,16 @@
     <div class="container">
         <div class="brand">
             <h1>Admin</h1>
+            
         </div>
 
         <nav>
             <ul>
-                <li><a href="index.php?act=home">Trang chủ</a></li>
-                <li><a href="index.php?act=categories">Danh mục</a></li>
-                <li><a href="index.php?act=prodList">Sản phẩm</a></li>
-                <li><a href="index.php?act=customers">Khách hàng</a></li>
-                <li><a href="index.php?act=comments">Bình luận</a></li>
-                <li><a href="index.php?act=report">Thống kê</a></li>
+                <li><i class="fas fa-home"></i> <a href="index.php?act=home">Trang chủ</a></li>
+                <li><i class="fas fa-layer-group"></i> <a href="index.php?act=categories">Danh mục</a></li>
+                <li><i class="fas fa-boxes"></i> <a href="index.php?act=prodList">Sản phẩm</a></li>
+                <li><i class="fas fa-users"></i> <a href="index.php?act=customers">Khách hàng</a></li>
+                <li><i class="far fa-comment"></i> <a href="index.php?act=comments">Bình luận</a></li>
+                <li><i class="fas fa-chart-bar"></i> <a href="index.php?act=report">Thống kê</a></li>
             </ul>
         </nav>
