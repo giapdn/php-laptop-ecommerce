38d7170d19e3c30f31bace6feee1fbca21e31a41
giapdn <donguyengiapicloud@gmail.com>
2023-11-20 22:40:16 +0700

    new update

--- index.php
+++ index.php
@@ -4,41 +4,23 @@ if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
     $act = $_GET['act'];
     switch ($act) {
 
-            case 'trangsanpham':
-                include "app/home/controllers/trangsanpham.php";
-    
-                break;   
-
-
-
-                case 'lienhe':
-                    include "app/home/controllers/lienhe.php";
-        
-                    break;   
-
-
-
-
-                    case 'tintuc':
-                        include "app/home/controllers/tintuc.php";
-                       
-                        break;   
-
-                        case 'giohang':
-                            include "app/home/controllers/giohang.php";
-                
-                            break;   
-    
-
-        
+        case 'trangsanpham':
+            include "app/home/controllers/trangsanpham.php";
+            break;
+        case 'lienhe':
+            include "app/home/controllers/lienhe.php";
+            break;
+        case 'tintuc':
+            include "app/home/controllers/tintuc.php";
+            break;
+        case 'giohang':
+            include "app/home/controllers/giohang.php";
+            break;
         default:
             # code...
             break;
     }
-}
-else {
+} else {
     include "app/home/controllers/body.php";
 }
 include "app/home/views/footer.php";
-?>
-
