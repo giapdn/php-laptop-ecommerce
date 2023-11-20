e47ef711c2c6b1b6c7a668d73f6ad2f96e70a618
quangson23 <nguyenquangson23022004@gmail.com>
2023-11-20 22:30:03 +0700

    update code

--- app/admin/models/database.php
+++ app/admin/models/database.php
@@ -1,9 +1,1 @@
-<?php
-$host = "localhost";
-$user = "root";
-$password = "root";
-$database = "data";
-$conn = new mysqli($host, $user, $password, $database);
-if ($conn->connect_error) {
-    die("Failed to connect: " . $conn->connect_error);
-}
+
--- index.php
+++ index.php
@@ -1,10 +1,35 @@
 <?php
 include "app/home/views/header.php";
-if (isset($_GET["act"])) {
-    switch ($variable) {
-        case 'value':
-            # code...
-            break;
+if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
+    $act = $_GET['act'];
+    switch ($act) {
+
+            case 'trangsanpham':
+                include "app/home/controllers/trangsanpham.php";
+    
+                break;   
+
+
+
+                case 'lienhe':
+                    include "app/home/controllers/lienhe.php";
+        
+                    break;   
+
+
+
+
+                    case 'tintuc':
+                        include "app/home/controllers/tintuc.php";
+                       
+                        break;   
+
+                        case 'giohang':
+                            include "app/home/controllers/giohang.php";
+                
+                            break;   
+    
+
         
         default:
             # code...
