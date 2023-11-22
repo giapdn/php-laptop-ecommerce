d5352a5eef87e3c63be0c7b44d8504fcf18bfec3
giapdn <donguyengiapicloud@gmail.com>
2023-11-22 22:55:46 +0700

    test

--- app/home/models/pdo.php
+++ app/home/models/pdo.php
@@ -3,9 +3,9 @@
  * Mở kết nối đến CSDL sử dụng PDO
  */
 function pdo_get_connection(){
-    $dburl = "mysql:host=localhost;dbname=shoplaptop;charset=utf8";
+    $dburl = "mysql:host=localhost;dbname=duan1;charset=utf8";
     $username = 'root';
-    $password = '';
+    $password = 'root';
 
     $conn = new PDO($dburl, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
